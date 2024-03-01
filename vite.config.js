fc851bf6ae8b9e603d76f96655c4103e05d7ad9c
Ciuca Veronica <veronicaciuca17@gmail.com>
2024-03-01 12:54:36 +0200

    config package json

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/qr-code-component-main-with-react-vite-scss/",
   plugins: [react()],
 })
